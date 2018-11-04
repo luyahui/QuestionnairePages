99711614a5ccada17755c761f2e7b2ec8fc57b2e
luyahui <luyh1090169180@gmail.com>
2018-11-04 10:00:02 -0500

    Minor change for html pages.

--- src/app/Question/question.service.ts
+++ src/app/Question/question.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
-const baseUrl = 'http://localhost:8090';
+const baseUrl = 'http://52.5.89.46:8090';
 
 @Injectable({
   providedIn: 'root'
